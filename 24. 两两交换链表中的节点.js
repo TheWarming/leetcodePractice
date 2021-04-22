710c2b41941a6698525d63bdda52b56c6f947e10
TheWarming <1175344627@qq.com>
2021-04-22 11:36:59 +0800

    d

---
+++ 24. 两两交换链表中的节点.js
@@ -0,0 +1,50 @@
+/* 24. 两两交换链表中的节点
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 
+
+示例 1：
+
+
+输入：head = [1,2,3,4]
+输出：[2,1,4,3] */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if(!head||!head.next) return head
+  let node = head
+  head = head.next
+  while(node && node.next){
+      const n = node.next
+      const nn = n.next
+      if(nn && nn.next){
+          node.next = nn.next
+      }else if(!nn){
+          node.next = null
+      }else{
+          node.next = nn
+      }
+      n.next = node
+      node = nn
+  }
+  return head
+};
