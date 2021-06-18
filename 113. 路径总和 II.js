d4afce90096d438fdb89c20f8eb1dbd8093324da
TheWarming <1175344627@qq.com>
2021-06-18 14:51:34 +0800

    d

---
+++ 113. 路径总和 II.js
@@ -0,0 +1,47 @@
+/* 给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+
+叶子节点 是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+输出：[[5,4,11,2],[5,8,4,5]]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/path-sum-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+  const temp = []
+  const ans = []
+  dfs(root,0)
+  return ans
+  function dfs(node,sum){
+      if(!node) return 
+      temp.push(node.val)
+      sum += node.val
+      if(sum === targetSum && !node.left && !node.right) {
+          ans.push([...temp])
+      }
+      dfs(node.left,sum)
+      dfs(node.right,sum)
+      temp.pop()
+  }
+
+};
