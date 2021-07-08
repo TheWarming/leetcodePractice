78d2096d7b480305337d83867e20ceef04f91af5
TheWarming <1175344627@qq.com>
2021-07-08 14:31:08 +0800

    d

---
+++ 面试题 10.11. 峰与谷.js
@@ -0,0 +1,18 @@
+/* 面试题 10.11. 峰与谷
+在一个整数数组中，“峰”是大于或等于相邻整数的元素，相应地，“谷”是小于或等于相邻整数的元素。例如，在数组{5, 8, 4, 2, 3, 4, 6}中，{8, 6}是峰， {5, 2}是谷。现在给定一个整数数组，将该数组按峰与谷的交替顺序排序。
+
+示例:
+
+输入: [5, 3, 1, 2, 3]
+输出: [5, 1, 3, 2, 3] */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var wiggleSort = function(nums) {
+  nums.sort((a,b) => b-a)
+  for(let i = 1; i < nums.length; i+=2){
+      [nums[i],nums[i-1]] = [nums[i-1],nums[i]]
+  }
+};
